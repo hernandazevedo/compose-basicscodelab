9c0a457d2e78c3ca158f1898ae97eca0c12bccbd
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-03-18 17:48:54 -0300

    Add cards

--- app/src/main/java/com/hernandazevedo/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/hernandazevedo/basicscodelab/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,13 +15,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
@@ -38,8 +36,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hernandazevedo.basicscodelab.R
 import com.hernandazevedo.basicscodelab.ui.theme.BasicsCodelabTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +51,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
+private fun MyApp() {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
@@ -64,82 +62,7 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        Column(modifier = Modifier.padding(4.dp)) {
-            LazyColumn {
-                item { Text("Header") }
-                items(names) { name ->
-                    Greeting(name)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-//    val extraPadding by animateDpAsState(
-//        targetValue = if (expanded) 48.dp else 0.dp,
-//        animationSpec = tween(durationMillis = 2000)
-//    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(8.dp, vertical = 4.dp)
-    ) {
-        Row(modifier = Modifier.padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
-            )) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-//                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello,")
-                Text(
-                    text = name, style = MaterialTheme.typography.h4.copy(
-                        fontWeight = FontWeight.ExtraBold
-                    )
-                )
-                if(expanded) {
-                    Text(
-                        text = stringResource(
-                            R.string.compose_ypseum
-                        )
-                    )
-                }
-            }
-//            OutlinedButton(onClick = { expanded = !expanded }) {
-//                Text(if (expanded) "Show less" else "Show more")
-//            }
-            IconButton(onClick = { expanded = !expanded }) {
-                if (expanded)
-                    Icon(imageVector = Icons.Filled.ExpandLess, contentDescription = stringResource(
-                        R.string.show_less)
-                    )
-                else
-                    Icon(imageVector = Icons.Filled.ExpandMore, contentDescription = stringResource(
-                                            R.string.show_more)
-                                        )
-            }
-        }
-    }
-}
-
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -157,22 +80,79 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
     }
 }
 
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    heightDp = 320,
-    uiMode = UI_MODE_NIGHT_YES
-)
 @Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {})
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
     }
 }
 
 @Preview(
-    showBackground = true, widthDp = 320, heightDp = 320
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
 )
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
@@ -180,14 +160,10 @@ fun DefaultPreview() {
     }
 }
 
-@Preview(
-    showBackground = true, widthDp = 320, heightDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun DefaultPreviewDark() {
+fun OnboardingPreview() {
     BasicsCodelabTheme {
-        Greetings()
+        OnboardingScreen(onContinueClicked = {})
     }
 }
