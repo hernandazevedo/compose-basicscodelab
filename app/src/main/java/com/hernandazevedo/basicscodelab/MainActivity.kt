4bfe7ca98455c59c7f4ed8c2b2dc474d89d738c7
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-03-18 17:02:56 -0300

    Adds animations

--- app/src/main/java/com/hernandazevedo/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/hernandazevedo/basicscodelab/MainActivity.kt
@@ -5,11 +5,15 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -19,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if(shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
@@ -49,15 +54,18 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+private fun Greetings(names: List<String> = List(1000) { "$it"}) {
     // A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         Column(modifier = Modifier.padding(4.dp)) {
-            for (name in names) {
-                Greeting(name)
+            LazyColumn {
+                item { Text("Header") }
+                items(names) { name->
+                    Greeting(name)
+                }
             }
         }
     }
@@ -66,7 +74,11 @@ private fun Greetings(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if(expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if(expanded) 48.dp else 0.dp,
+        animationSpec = tween( durationMillis = 2000)
+    )
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier
